8c5b3af01f1d71450768ff783fe497c2f0e17244
M4hf0d <ma.sayah@esi-sba.dz>
2022-05-15 10:35:05 +0200

    updated urls

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -11,7 +11,8 @@ class StreamPlatform(models.Model):
 class WatchList(models.Model):
     title = models.CharField(max_length=50) 
     storyline = models.CharField(max_length=200)
-    platform = models.ForeignKey(StreamPlatform , on_delete=models.CASCADE, related_name = 'watchlist')
+    # platform = models.ForeignKey(StreamPlatform , on_delete=models.CASCADE, related_name = 'watchlist')
+    platform = models.ManyToManyField(StreamPlatform)
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
